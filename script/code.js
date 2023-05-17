2c702719438bb7d5d6489aad6318b340a0f1f9c7
siyamanga22 <zwenisiyamanga@gmail.com>
2023-05-17 08:57:12 +0200

    done

---
+++ script/code.js
@@ -0,0 +1,11 @@
+ let name = 'Siyamanga'
+ console.log (typeof name);
+ name = 2 ;
+ console.log(typeof name); //it changes to number
+ let person ={
+    firstName: 'zweni',
+    lastName :'zwezwe',
+    age: 22
+
+}
+console.log(typeof person)//displayed object
